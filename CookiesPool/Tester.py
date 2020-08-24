6d5e1e88758c0560094ffdc8213e925454cede65
SenQi-666 <57979568+SenQi-666@users.noreply.github.com>
2020-08-24 14:37:33 +0800

    Update Tester.py

--- CookiesPool/Tester.py
+++ CookiesPool/Tester.py
@@ -10,8 +10,9 @@ class BasicTester:
         self.DB_ACCOUNT = RedisClient(website, 'Account')
         self.DB_COOKIES = RedisClient(website, 'Cookies')
         self.TEST_DELAY = Settings.TEST_DELAY
+        self.STATUS = False
 
-    def test(self, usr, cookies, test_url):
+    def test(self, cookies,  usr, test_url):
         print('账号：%s，正在检测Cookies......' % usr)
         try:
             cookies = json.loads(cookies)
@@ -20,9 +21,11 @@ class BasicTester:
                 cookie_dict[key] = str(val)
             res = requests.get(test_url, cookies=cookie_dict, allow_redirects=False)
             if res.status_code == 200:
+                self.STATUS = True
                 print(cookie_dict)
                 print('账号：%s Cookies有效' % usr)
             elif res.status_code == 302:
+                self.STATUS = False
                 print('账号：%s Cookies失效，删除cookies' % usr)
                 self.DB_COOKIES.delete(usr)
             else:
@@ -30,6 +33,7 @@ class BasicTester:
         except json.JSONDecodeError:
             print('账号：%s，Cookie无效' % usr)
             self.DB_COOKIES.delete(usr)
+            self.STATUS = False
             print('账号：%s，Cookie已删除' % usr)
         except ConnectionError as e:
             print('连接异常', e.args)
@@ -44,5 +48,5 @@ class WeiBoCookiesTester(BasicTester):
         test_url = Settings.TEST_URL[self.website]
         cookies_group = self.DB_COOKIES.all()
         for usr, cookies in cookies_group.items():
-            self.test(usr, cookies, test_url)
+            self.test(cookies, usr, test_url)
             time.sleep(self.TEST_DELAY)
