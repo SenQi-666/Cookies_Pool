88039985fdb43945079104b6926538caf82a84a4
SenQi-666 <57979568+SenQi-666@users.noreply.github.com>
2020-08-28 14:40:50 +0800

    Update Generator.py

--- CookiesPool/Generator.py
+++ CookiesPool/Generator.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from CookiesPool.RedisControl import RedisClient
@@ -6,7 +7,6 @@ from CookiesPool.weibo.PhoneSMS import SMS
 from selenium import webdriver
 from conf import Settings
 import time
-import json
 
 
 class BasicGenerator:
@@ -23,6 +23,9 @@ class BasicGenerator:
     def save_cookie(self, usr, cookies):
         self.DB_COOKIES.set(usr, cookies)
 
+    def delete_account(self, usr):
+        self.DB_ACCOUNT.delete(usr)
+
     def generate(self, *args):
         raise SyntaxError('Please overwrite the <Function generate>')
 
@@ -36,15 +39,12 @@ class WeiBoCookiesGenerator(BasicGenerator):
         super(WeiBoCookiesGenerator, self).__init__(website)
         self.website = website
         self.phone_number = phone_umber
-        try:
-            options = webdriver.ChromeOptions()
-            options.add_experimental_option('excludeSwitches', ['enable-automation'])
-            self.driver = webdriver.Chrome(options=options)
-            self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-                "source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})""",
-            })
-        except Exception:
-            raise Exception('Please configure the Chrome first')
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-automation'])
+        self.driver = webdriver.Chrome(options=options)
+        self.driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+            "source": """Object.defineProperty(navigator, 'webdriver', {get: () => undefined})""",
+        })
 
     def generate(self, account, phone_number):
         usr, pwd = account, self.DB_ACCOUNT.get(account)
@@ -56,6 +56,11 @@ class WeiBoCookiesGenerator(BasicGenerator):
         self.driver.find_element_by_id('loginPassword').send_keys(pwd)
         self.driver.find_element_by_id('loginAction').click()
         time.sleep(3)
+        error_msg = self.driver.find_elements_by_id('errorMsg')
+        if error_msg:
+            self.delete_account(usr)
+            print('用户名或密码错误，账号已于库中删除')
+            return
         if self.driver.find_elements_by_class_name('geetest_radar_tip'):
             print('请人工点击认证或滑动滑块')
             wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'nav_item')))
@@ -72,10 +77,12 @@ class WeiBoCookiesGenerator(BasicGenerator):
             time.sleep(2)
             self.driver.find_element_by_xpath('//*[@id="msgVerify"]/div[1]/div/div/div[2]/a[2]').click()
         cookies_list = self.driver.get_cookies()
+        print(cookies_list)
         cookie = [item["name"] + "=" + item["value"] for item in cookies_list]
         cookies = '; '.join(item for item in cookie)
         print(cookies)
         self.save_cookie(usr, cookies)
+        print('账号：%s 生成Cookie成功！已保存！' % usr)
 
     def run(self):
         accounts = self.get_account()
@@ -83,6 +90,5 @@ class WeiBoCookiesGenerator(BasicGenerator):
             if account not in self.DB_COOKIES.all_usr():
                 print('正在为账号：%s 生成Cookie......请稍后...' % account)
                 self.generate(account, self.phone_number)
-                print('账号：%s 生成Cookie成功！已保存！' % account)
                 time.sleep(self.GENERATOR_DELAY)
         self.driver.close()
